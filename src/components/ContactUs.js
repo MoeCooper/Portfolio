e742779bb36feba27d380acf2b316de81bb9d971
Maurice <42690947+MoeCooper@users.noreply.github.com>
2020-03-16 02:17:26 -0500

    Add files via upload
    Completed Portfolio

---
+++ src/components/ContactUs.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              <h1><a href="mailto:jc70240@gmail.com? subject=Questions Comments or Concerns">Click Here To Email Me</a></h1>
+              </p>
+            </div>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,71 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/MoeCooper",
+    "name": "Maurice Cooper",
+    "role": "I am a Fullstack Developer",
+    "linkedinId":"https://www.linkedin.com/in/mauricecooper/",
+    "roleDescription": " and an Air Force Veteran who loves to constantly learn about programming technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/mauricecooper/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/MoeCooper",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/Maurice19266591",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently enrolled in the Computer Science Bachelor's Degree program. I have completed Helio Training Coding Bootcamp.",
+    "address":"USA",
+    "education":[
+      {
+        "UniversityName":"Grantham University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"February",
+        "YearOfPassing":"2020"
+      },
+      {
+        "UniversityName":"Helio Training Coding Bootcamp",
+        "specialization":"FullStack Web Development",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2020"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Welcome Home",
+        "specialization":"Shelter for homeless veterans",
+        "MonthOfLeaving":"Currently Work Here",
+        "YearOfLeaving":"2019"
+      },
+      {
+        "CompanyName":"United States Air Force",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2019"
+       
+      }
+    ],
+    "skillsDescription":"HTML, CSS, JS, React.js, Express.js, Node.js, MongoDB, SQL, C++, Python",
+    "portfolio":[
+      {
+        "name":"project1",
+        
+      },
+      {
+        "name":"project2",
+        
+      },
+      {
+        "name":"project3",
+        
+      }
+    ],
+  }
+  
+  export default resumeData
